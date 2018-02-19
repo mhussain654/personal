4abeaefbc951a62222f83d1d89d407e7147a7449
Hussain <mhussain654@gmail.com>
2018-02-19 14:26:33 +0500

    personal page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'personal/index'
+
   get 'project_detail/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "project_detail#index"
+  root "personal#index"
 end
